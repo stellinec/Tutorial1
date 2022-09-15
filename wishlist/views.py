ec1d7664eb4bd078844a9bcc2f52d76dda433468
Stelline Claudia <stellineclaudia32@gmail.com>
2022-09-15 14:06:46 +0700

    .

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
-
+from django.http import HttpResponse
+from django.core import serializers
 # Create your views here.
 data_barang_wishlist = BarangWishlist.objects.all()
+
 context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Stelline'
 }
 def show_wishlist(request):
     return render(request, "wishlist.html", context)
+def show_xml(request):
+    return HttpResponse(serializers.serialize("xml", data_barang_wishlist), content_type="application/xml")
+def show_json(request):
+    return HttpResponse(serializers.serialize("json", data_barang_wishlist), content_type="application/json")
+def show_json_by_id(request,  id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    
